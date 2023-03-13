4b5da0aabebd9a57061caf387d9c54b915289915
Jim Wang <wangj3743@wrdsb.ca>
2023-03-12 22:53:08 -0400

    more set up

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,70 @@
 </script>
 
 
-
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="m-auto w-[70%]">
+  <!--section 00: header-->
+    <div> 
+      <img class="rounded-[100%] scale-75" src="src\assets\cube.jpg">
+      <h1 class="text-[80px] font-bold">
+        Jim Wang
+      </h1>
+    </div>
+    <hr>
+  <!--section 01: about    2 5 8-->
+    <div> 
+      <h2 class="text-[40px] font-bold">about</h2>
+      <p>hi my name is jim</p>
+      <p>
+        Lorem ipsum dolor 
+        sit amet consectetur 
+        adipisicing elit. 
+        Iusto, sed repellendus? 
+        Quo facilis laboriosam 
+        pariatur qui rem 
+        perferendis nesciunt
+        obcaecati corporis
+        ullam eaque deleniti
+        sed quibusdam, alias 
+        autem sequi enim?
+      </p>
+    </div>
+    <hr>
+  <!--section 02: projects-->
+    <div> 
+      <h2 class="text-[40px] font-bold">projects</h2>
+      <p>
+        Lorem ipsum dolor 
+        sit amet consectetur 
+        adipisicing elit. 
+        Iusto, sed repellendus? 
+        Quo facilis laboriosam 
+        pariatur qui rem 
+        perferendis nesciunt
+        obcaecati corporis
+        ullam eaque deleniti
+        sed quibusdam, alias 
+        autem sequi enim?
+      </p>
+    </div>
+    <hr>
+  <!--section -1: footer/contact-->
+    <div> 
+      <h2 class="text-[40px] font-bold">contact</h2>
+      <p>
+        Lorem ipsum dolor 
+        sit amet consectetur 
+        adipisicing elit. 
+        Iusto, sed repellendus? 
+        Quo facilis laboriosam 
+        pariatur qui rem 
+        perferendis nesciunt
+        obcaecati corporis
+        ullam eaque deleniti
+        sed quibusdam, alias 
+        autem sequi enim?
+      </p>
+    </div>
+    <br><br><br><br><br><br><br><br>
+  </div>
 </template>
